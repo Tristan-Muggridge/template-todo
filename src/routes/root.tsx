34e53fb6c9d2baed862d1015de51020597ea52f3
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-10-24 16:15:02 +1000

    styling update

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -6,35 +6,54 @@ import { IoLanguageSharp } from "react-icons/io5";
 import { useLanguage } from '../context/LanguageContext';
 import { SupportedLanguages } from "../UILanguages";
 
+const LanguageToggle = () => {
+    
+    const { language, setLanguage } = useLanguage();
+
+    useEffect(() => {
+        document.title = language._id === SupportedLanguages.English ? 'Template Todo' : 'マイ・タスク';
+    }, [language]);
+    
+    return (
+        <button className=" text-center w-full flex items-center justify-center mx-auto" onClick={() => {
+            language._id === SupportedLanguages.English ? setLanguage(SupportedLanguages.Japanese) : setLanguage(SupportedLanguages.English);
+        }}> 
+            <IoLanguageSharp /> 
+        </button>
+    )
+}
+
 const SideNav = () => {
     
     const [expanded, setExpanded] = useState(false);
     const [page, setPage] = useState(window.location.href.split('/').pop());
-    const { language, setLanguage } = useLanguage();
 
     const onNavClick = () => {
         setExpanded(false);
         setPage(window.location.href.split('/').pop());
     }
 
-    useEffect(() => {
-        document.title = language._id === SupportedLanguages.English ? 'Template Todo' : 'マイ・タスク';
-    }, [language])
+    const { language } = useLanguage();
 
     return (
-        <nav className="w-28 supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh] fixed z-10">
+    <nav className={`flex flex-col w-12`}>
+        <span className="bg-neutral-900 p-4 text-2xl flex items-center justify-center"
+         onClick={() => setExpanded(!expanded)}>    
+            <BsLayoutSidebar /> 
+        </span>
+        
+        <div className={`
+            flex flex-col gap-4 w-full h-full transition-all duration-200
+        `}>
             <div className={`
-                h-full flex flex-col justify-between items-center w-24 transition-all duration-200 relative
-            `}>
+                flex flex-col gap-4 w-12 fixed top-12 bottom-0 left-0 bg-neutral-900 transition-all duration-200
+                ${expanded ? '-left-0' : '-left-12'}
+                justify-between items-center py-4 text-2xl
                 
-                <span className="bg-neutral-900 p-4 flex justify-center items-center shadow-lg shadow-black transition-all duration-200 w-full absolute top-0 left-0" onClick={() => setExpanded(!expanded)}> <BsLayoutSidebar /> </span>
-
-                <div className={`
-                    bg-neutral-900 p-4 shadow-lg shadow-black transition-all duration-200 grow justify-between flex flex-col w-full absolute top-10 bottom-0
-                    ${expanded ? 'left-0' : '-left-24 md:-left-0'}
-                `}>
+                z-10
+            `}>
                     <div/>
-                    <div className="flex flex-col text-2xl items-center gap-24 w-full">
+                    <div className="flex flex-col gap-8">
                         {
                             [
                                 {
@@ -61,12 +80,11 @@ const SideNav = () => {
                         }
                     </div>
 
-                    <button className="text-2xl flex justify-center" onClick={() => {
-                        language._id === SupportedLanguages.English ? setLanguage(SupportedLanguages.Japanese) : setLanguage(SupportedLanguages.English);
-                    }}> <IoLanguageSharp /> </button>
-                </div>
+                    <LanguageToggle />
             </div>
-        </nav>
+
+        </div>
+    </nav>
     )
 }
 
